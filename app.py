828d88a396deb00248112c8b55523ccd6ae78382
harshkarn09 <135315745+harshkarn09@users.noreply.github.com>
2025-05-23 22:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,23 +13,33 @@ movies = pd.DataFrame(movies_dict)
 with gzip.open('similarity.pkl.gz', 'rb') as f:
     similarity = pickle.load(f)
 
+# Fetch poster using TMDB API
 def fetch_poster(movie_id):
-    response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=a95299e81ac22292d76c6b3e83e2c5b0&language=en-US")
-    data = response.json()
-    poster_path = data.get('poster_path', '')
-    if poster_path:
-        return "https://image.tmdb.org/t/p/w500/" + poster_path
-    return "https://via.placeholder.com/500"
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=a95299e81ac22292d76c6b3e83e2c5b0&language=en-US"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+    except Exception as e:
+        print(f"Error fetching poster for movie_id={movie_id}: {e}")
+    return "https://via.placeholder.com/500?text=No+Image"
 
+# Recommend function
 def recommend(movie):
+    if movie not in movies['title'].values:
+        return [], []
+
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_movies_posters = []
-    for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
+    for i in movie_indices:
+        movie_id = movies.iloc[i[0]]['movie_id']
         recommended_movies.append(movies.iloc[i[0]]['title'])
         recommended_movies_posters.append(fetch_poster(movie_id))
 
@@ -37,16 +47,22 @@ def recommend(movie):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    selected_movie = None
     recommended = []
     posters = []
-    selected_movie = None
 
     if request.method == 'POST':
-        selected_movie = request.form['movie']
-        recommended, posters = recommend(selected_movie)
+        selected_movie = request.form.get('movie')
+        if selected_movie:
+            recommended, posters = recommend(selected_movie)
 
     zipped = zip(recommended, posters)
-    return render_template('index.html', movies=movies['title'].values, zipped=zipped, selected_movie=selected_movie)
+    return render_template(
+        'index.html',
+        movies=movies['title'].values,
+        zipped=zipped,
+        selected_movie=selected_movie
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
