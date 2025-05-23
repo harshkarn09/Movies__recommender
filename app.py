6a795567989aec0c4af2d7a13774934faad10f60
harshkarn09 <harshkarn50@gmail.com>
2025-05-23 20:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
-import streamlit as st  # Add this
 import requests
 
+app = Flask(__name__)
+
+# Load data
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
 def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}"
- "?api_key=a95299e81ac22292d76c6b3e83e2c5b0&language=en-US".format(movie_id))
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=a95299e81ac22292d76c6b3e83e2c5b0&language=en-US")
     data = response.json()
-    poster_path = data.get('poster_path', '')  # Use .get() to avoid KeyError
+    poster_path = data.get('poster_path', '')
     if poster_path:
-        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-        return full_path
-    return "https://via.placeholder.com/500"  # Placeholder for missing posters
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    return "https://via.placeholder.com/500"
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -22,50 +27,22 @@ def recommend(movie):
     recommended_movies_posters = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-
-        recommended_movies.append(movies.iloc[i[0]]['title'])  # Fixed indexing
-        # fetch poster from API
+        recommended_movies.append(movies.iloc[i[0]]['title'])
         recommended_movies_posters.append(fetch_poster(movie_id))
 
     return recommended_movies, recommended_movies_posters
 
-# Load movie dictionary
-movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    recommended = []
+    posters = []
+    selected_movie = None
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+    if request.method == 'POST':
+        selected_movie = request.form['movie']
+        recommended, posters = recommend(selected_movie)
 
-# Streamlit UI
-st.title('Movie Recommender System')
+    return render_template('index.html', movies=movies['title'].values, recommended=recommended, posters=posters, selected_movie=selected_movie)
 
-option = st.selectbox(
-    'How would you like to be contacted?',
-    movies['title'].values
-)
-
-names = []
-posters = []
-if st.button('Recommend'):
-    names, posters = recommend(option)  # Fixed undefined variable
-
-col1, col2, col3, col4, col5 = st.columns(5)
-
-# **Fix IndexError by handling missing values gracefully**
-try:
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-except IndexError:
-    pass  # If there are fewer than 5 recommendations, this prevents crashing.
+if __name__ == '__main__':
+    app.run(debug=True)
